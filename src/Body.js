51d391e3daac8d1f448927424fd78d85c4b25f45
rishab-talla <connect.rishab.talla.com>
2025-07-07 22:47:02 +0530

    added dynamic filtering on the basis of toggle status and also added search feature and remove feature

--- src/Body.js
+++ src/Body.js
@@ -5,46 +5,96 @@ const Body = () => {
     const [allFilter, setAllFilter] = useState("yes");
     const [activeFilter, setActiveFilter] = useState("no");
     const [inactiveFilter, setInactiveFilter] = useState("no");
-    const [filteredData, setFilteredData] = useState(data);
+
+    const [cardsData, setCardsData] = useState(data);
+    const [filterTab, setFilterTab] = useState("All");
+
+    const[searchText, setSearchText] = useState("");
+
+    const handleToggle = (name) => {
+        const updated = cardsData.map((card) => {
+        return card.name === name ? {...card, isActive:!card.isActive} : card;
+        });
+        
+        setCardsData(updated);
+    }
+
+    const handleRemoveButton = (name) => {
+        const updated = cardsData.filter((card) => {
+            return card.name !== name;
+        });
+
+        setCardsData(updated);
+    }
+
+    const filteredData = cardsData.filter((card) => {
+        if(filterTab === "Active") return card.isActive;
+        if(filterTab === "Inactive") return !card.isActive;
+        return true;
+    })
 
     return (
         <div className="Body">
             <div className="Body-head ">
                 <div className="Body-head-left">Extensions List</div>
                 <ul className="Body-head-right ">
+                    <li >
+                        <input onChange={(e) => {
+                            setSearchText(e.target.value);
+                        }}
+                        style={{color:"white",background:"rgb(40, 59, 85)",padding:".3rem .6rem",border:"none", borderRadius:".5rem",
+                            marginRight:".5rem", fontWeight:"bold"
+                        }}
+                         placeholder="search for extentions..." />
+                        <i
+                        onClick={() => {
+                            const searchData = cardsData.filter((card) => {
+                                return card.name.toLowerCase().includes(searchText.toLowerCase());
+                            })
+
+                            setCardsData(searchData);
+                            setAllFilter("no");
+                            setActiveFilter("no");
+                            setInactiveFilter("no");
+                        }} 
+                        style={{marginRight:".6rem"}} className="fa-solid fa-magnifying-glass"> </i>
+                    </li>
+
                     <li onClick={() => {
-                        if(allFilter === "no") {
+
                             setAllFilter("yes");
                             setActiveFilter("no");
                             setInactiveFilter("no");
-                            setFilteredData(data);
-                        }
+                            setFilterTab("All");
+                            setCardsData(data);
+                        
                     }}
                      style={{background:allFilter === "yes" ? "rgb(255, 0, 0)" : "rgb(40, 59, 85)"}}>All</li>
 
                     <li onClick={() => {
-                        if(activeFilter === "no") {
                             
-                            const filter = data.filter((card) => {
-                                return card.isActive === true;
-                            })
-                            setFilteredData(filter);
+                            // const filter = data.filter((card) => {
+                            //     return card.isActive === true;
+                            // })
                             setActiveFilter("yes");
                             setAllFilter("no");
                             setInactiveFilter("no");
-                        }
+                            setFilterTab("Active");
+                            setCardsData(data);
+                        
                     }} style={{background:activeFilter === "yes" ? "rgb(255, 0, 0)" : "rgb(40, 59, 85)" }}>Active</li>
 
                     <li onClick={() => {
-                        if(inactiveFilter === "no") {
-                            const filter = data.filter((card) => {
-                                return card.isActive === false;
-                            })
-                            setFilteredData(filter);
+                            // const filter = data.filter((card) => {
+                            //     return card.isActive === false;
+                            // })
+                            
                             setInactiveFilter("yes");
                             setAllFilter("no");
                             setActiveFilter("no");
-                        }
+                            setFilterTab("Inactive");
+                            setCardsData(data);
+
                     }} style={{background:inactiveFilter === "yes" ? "rgb(255, 0, 0)" : "rgb(40, 59, 85)" }}>Inactive</li>
                 </ul>
             </div>
@@ -52,7 +102,11 @@ const Body = () => {
                 {filteredData.map((card) => {
                     return (
                     <ExtensionCard
-                    cardData = {card}/>  
+                    key = {card.name}
+                    cardData = {card}
+                    hasToggled = {handleToggle}
+                    removeButton = {handleRemoveButton}
+                    />  
                     )  
                 })}
                 
--- src/ExtensionCard.js
+++ src/ExtensionCard.js
@@ -1,12 +1,12 @@
 
-import Toggle from "./Toggle";
 const ExtensionCard = (props) => {
-    const {cardData} = props;
+    const {cardData, hasToggled, removeButton} = props;
     const {logo, name, description, isActive} = cardData;
     return (
         <div className="Card backgroundColor">
             <div className="Card-top">
                     <div className="card-img">
+                        
                         <img src={logo} />
                     </div>
                     <div className="card-details">
@@ -15,9 +15,12 @@ const ExtensionCard = (props) => {
                     </div>
                 </div>
                 <div className="card-bottom">
-                    <button className="card-botton-left">Remove</button>
-                    <div style={{display:"inline-flex",marginLeft:"10rem"}}>
-                    <Toggle/>
+                    <button onClick={() => removeButton(name)} className="card-botton-left">Remove</button>
+                    <div onClick={() => hasToggled(name) }
+                     className ={` toggle1 ${isActive ? "bg-orange" : "bg-gray"} `} style={{display:"inline-flex",marginLeft:"10rem"}}>
+
+                        <div className={`toggle2 ${isActive ? "translate-x-5" : ""}`}></div>
+
                     </div>
                 </div>
         </div>
